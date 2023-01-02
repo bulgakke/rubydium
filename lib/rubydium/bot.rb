3e38d68ba884fab850abf1213517a254ae44f13e
Evgeniy Bulavin <vvp835@yandex.ru>
2023-01-02 08:20:58 +0400

    Implement on_command

--- lib/rubydium/bot.rb
+++ lib/rubydium/bot.rb
@@ -5,16 +5,26 @@ module Rubydium
   # This is a base class that's meant to be inherited by user's actual bot class,
   # where actual logic of handling the updates is described.
   class Bot
+    include Mixins
+
+    COMMAND_REGEXP = %r{\A/}
+
     def self.run
       Telegram::Bot::Client.run(config.token) do |client|
         client.listen do |update|
-          new(client, update).handle_update
+          # Not all `update`s here are messages (`listen` yields `Update#current_message`,
+          # which can be `ChatMemberUpdated` etc.)
+          # TODO: rework to allow for clean handling of other types.
+          if update.is_a? Telegram::Bot::Types::Message
+            new(client, update).handle_update
+          end
         end
       end
     end
 
     def initialize(client, update)
       @client = client
+      @api = client.api
       @msg = update
       @user = @msg.from
       @chat = @msg.chat
@@ -26,15 +36,19 @@ module Rubydium
     end
 
     def handle_update
-      # Stuff happens here
+      execute_command
     end
 
     private
 
+    # This assumes the message starts with the `/command`.
+    # For example:
+    # "/start asdf", "/start@yourbot", "/start /another_command", "asdf /start" will all return "/start".
     def get_command(text)
       return unless text
 
-      text.split[0].delete_suffix(Config.bot_username)
+      command = text.split.grep(COMMAND_REGEXP).first
+      command&.delete_suffix(config.bot_username)
     end
   end
 end
