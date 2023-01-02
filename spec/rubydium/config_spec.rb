c50111b20e6b56435ff711463249fd29c36b4de5
Evgeniy Bulavin <vvp835@yandex.ru>
2023-01-03 03:27:04 +0400

    Rework creating test bot classes and instances

--- spec/rubydium/config_spec.rb
+++ spec/rubydium/config_spec.rb
@@ -3,7 +3,13 @@
 require "json"
 
 RSpec.describe Rubydium::Config do
-  let(:test_bot_class) { Class.new(Rubydium::Bot) }
+  let(:message) { create_message(text: "/start") }
+  let(:client) { instance_double(Telegram::Bot::Client) }
+  let(:bot_class) { create_bot_class }
+  let(:bot) do
+    allow(client).to receive :api
+    bot_class.new(client, message)
+  end
   let(:test_config) do
     {
       bot_username: "@your_bot",
@@ -15,46 +21,46 @@ RSpec.describe Rubydium::Config do
   end
 
   before(:example) do
-    test_bot_class.configure do |config|
+    bot_class.configure do |config|
       config.bot_username = nil
       config.privileged_usernames = nil
     end
   end
 
   it "sets config values using block syntax" do
-    expect(test_bot_class.config.bot_username).to be nil
-    expect(test_bot_class.config.privileged_usernames).to be nil
+    expect(bot_class.config.bot_username).to be nil
+    expect(bot_class.config.privileged_usernames).to be nil
 
-    test_bot_class.configure do |config|
+    bot_class.configure do |config|
       config.bot_username = "@your_bot"
       config.privileged_usernames = %w[@user_one @user_two]
     end
 
-    expect(test_bot_class.config.bot_username).to eq("@your_bot")
-    expect(test_bot_class.config.privileged_usernames).to eq(%w[@user_one @user_two])
+    expect(bot_class.config.bot_username).to eq("@your_bot")
+    expect(bot_class.config.privileged_usernames).to eq(%w[@user_one @user_two])
   end
 
   it "sets config values using hash syntax" do
-    expect(test_bot_class.config.bot_username).to be nil
-    expect(test_bot_class.config.privileged_usernames).to be nil
+    expect(bot_class.config.bot_username).to be nil
+    expect(bot_class.config.privileged_usernames).to be nil
 
-    test_bot_class.config = test_config
+    bot_class.config = test_config
 
-    expect(test_bot_class.config.bot_username).to eq("@your_bot")
-    expect(test_bot_class.config.privileged_usernames).to eq(%w[@user_one @user_two])
+    expect(bot_class.config.bot_username).to eq("@your_bot")
+    expect(bot_class.config.privileged_usernames).to eq(%w[@user_one @user_two])
   end
 
   it "sets config values from an external file" do
     require "json"
 
-    expect(test_bot_class.config.bot_username).to be nil
-    expect(test_bot_class.config.privileged_usernames).to be nil
+    expect(bot_class.config.bot_username).to be nil
+    expect(bot_class.config.privileged_usernames).to be nil
 
     json = test_config.to_json
     File.write("#{TEMP_DIR}/config.json", json)
-    test_bot_class.config = JSON.load_file("#{TEMP_DIR}/config.json")
+    bot_class.config = JSON.load_file("#{TEMP_DIR}/config.json")
 
-    expect(test_bot_class.config.bot_username).to eq("@your_bot")
-    expect(test_bot_class.config.privileged_usernames).to eq(%w[@user_one @user_two])
+    expect(bot_class.config.bot_username).to eq("@your_bot")
+    expect(bot_class.config.privileged_usernames).to eq(%w[@user_one @user_two])
   end
 end
--- spec/rubydium/mixins/command_macros_spec.rb
+++ spec/rubydium/mixins/command_macros_spec.rb
@@ -1,44 +1,34 @@
 # frozen_string_literal: true
 
 RSpec.describe Rubydium::Mixins::CommandMacros do
-  let(:test_bot_class) do
-    bot = Class.new(Rubydium::Bot)
-    bot.configure do |config|
-      config.bot_username = "asdf"
-    end
-    bot
-  end
-
+  let(:message) { create_message(text: "/test_command") }
   let(:client) { instance_double(Telegram::Bot::Client) }
-  let(:update) do
-    data = { message: { text: "/test_command" } }
-    Telegram::Bot::Types::Update.new(data).current_message
+  let(:bot_class) { create_bot_class }
+  let(:bot) do
+    allow(client).to receive :api
+    bot_class.new(client, message)
   end
 
   describe ".on_command" do
     it "registers commands with a block" do
-      test_bot_class.define_method(:test_method) { }
+      bot_class.define_method(:test_method) { }
       block = proc do
         test_method
       end
-      test_bot_class.on_command "/test_command", &block
+      bot_class.on_command "/test_command", &block
 
-      expect(test_bot_class.registered_commands).to include("/test_command")
+      expect(bot_class.registered_commands).to include("/test_command")
 
-      allow(client).to receive(:api)
-      bot = test_bot_class.new(client, update)
       expect(bot).to receive(:test_method)
       bot.handle_update
     end
 
     it "registers commands with a method name" do
-      test_bot_class.define_method(:method_name) { }
-      test_bot_class.on_command "/test_command", :method_name
+      bot_class.define_method(:method_name) { }
+      bot_class.on_command "/test_command", :method_name
 
-      expect(test_bot_class.registered_commands).to include("/test_command")
+      expect(bot_class.registered_commands).to include("/test_command")
 
-      allow(client).to receive(:api)
-      bot = test_bot_class.new(client, update)
       expect(bot).to receive(:method_name)
       bot.handle_update
     end
